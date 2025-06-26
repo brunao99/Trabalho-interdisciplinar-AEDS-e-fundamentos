0285dda83fa7200431615684d7321cc8d08de1ac
Bruno <brunohenriquexavier99@gmail.com>
2025-06-25 22:18:32 -0300

    feat: Criação do struct dos produtos

--- E_commerce_Trabalho/main.c
+++ E_commerce_Trabalho/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void CriarProduto(){
-
-
-
-}
-
+typedef struct new_produtos{
+    char nome_produto[50];
+    int codigo_produto;
+    int qnt_estoque_produto;
+    int preco_venda_produto;
+}produtos;
 
 int main()
 {
